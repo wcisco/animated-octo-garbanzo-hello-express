a3a9bef1b8d3c81016353ec44ba28eecf0b5ecab
Cisco <wcisco@una.edu>
2025-08-26 13:26:56 -0500

    commit

--- app.mjs
+++ app.mjs
@@ -2,8 +2,17 @@ import express from 'express'
 
 const app = express()
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
-  res.send('Hello Express')
+  res.send('Hello Express from Render')
 })
 
-app.listen(3000)
+// endpoints... middlewares... apis?
+// send an html file
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
+
+// TODO: refactor to use env port.
